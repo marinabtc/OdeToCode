f019f4c6ff44ec0357b93297662e561101cb67ef
vcdsc <117476319+vcdsc@users.noreply.github.com>
2023-02-14 15:03:25 +0000

    TO REVIEW JOINTLY BEFORE MERGING
    Removed "find me a poem" random link from navbar and turned into a "random poem" button in advanced search bar sectio instead. Reused Marina's logic for displaying the poem inside the image card (renderPoem) to display the random poem as well.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,40 +8,26 @@ var linecount;
 $("#search-button").on("click", getPoem);
 
 // When the enter key is pressed is clicked gets the poem corresponding to the input search
-$(".searchbar-whole").on("keyup", function (event) {
+$("#main-input").on("keyup", function (event) {
   if (event.key === "Enter" || event.keyCode === 13) {
     getPoem();
-
   }
 });
-
-// When "Advanced search" is clicked remove anything in the main search box
-$(".adv-search").on("click", function () {
-
-  $("#main-input").val("");
-})
 
 function getPoem() {
   // Grabs the value of the search by option
   var selected = $(".form-select").val();
 
-  // Grabs the value of the main user input
-  var mainInput = $("#main-input").val().trim();
+  // Grabs the value of the user input
+  var input = $("#main-input").val().trim();
 
-  // Grabs the values of the advance search
-  var advAuthor = $("#adv-author").val().trim();
-  var advTitle = $("#adv-title").val().trim();
-  var advLines = $("#adv-lines").val().trim();
-
-  console.log("INPUT is: " + mainInput);
-  console.log("Advance search is: " + advAuthor, advTitle, advLines);
-
+  console.log("INPUT is: " + input);
 
   // Only executes if the input is not empty
-  if (mainInput !== "") {
+  if (input !== "") {
     // If search by "author" is selected
     if (selected == 1) {
-      author = mainInput;
+      author = input;
 
       //replaces spaces by % to be supported in the URL
       author.replace(" ", "%");
@@ -51,7 +37,7 @@ function getPoem() {
 
     // If search by "title" is selected
     else if (selected == 2) {
-      title = mainInput;
+      title = input;
       title.replace(" ", "%");
       console.log(title);
 
@@ -60,7 +46,7 @@ function getPoem() {
 
     // If search by "lines" is selected
     else if (selected == 3) {
-      lines = mainInput;
+      lines = input;
       lines.replace(" ", "%");
       console.log(lines);
 
@@ -77,112 +63,31 @@ function getPoem() {
       url: queryURL,
       method: "GET",
     }).then(function (response) {
+      if (response !== null) {
+        // console.log(response);
 
-      if (typeof response === Array) {
-        console.log(response);
+        // // gets the poem's title
+        // var poemTitle = response[0].title;
+        // console.log("Title: " + poemTitle);
 
-        // gets the poem's title
-        var poemTitle = response[0].title;
-        console.log("Title: " + poemTitle);
+        // // gets the poem's author
+        // var poemAuthor = response[0].author;
+        // console.log("Author: " + poemAuthor);
 
-// <<<<<<< feature-searchby-modal
-        // If nothing is selected
-        else {
-            // alert("Please select a search parameter.")
-            $("#no-search-by").modal("show");
-=======
-        // gets the poem's author
-        var poemAuthor = response[0].author;
-        console.log("Author: " + poemAuthor);
+        // // gets the poem's lines
+        // var poemLines = response[0].lines;
 
-        // gets the poem's lines
-        var poemLines = response[0].lines;
+        // console.log("Lines: ");
 
-        console.log("Lines: ");
+        // // loops through the lines array
+        // for (var i = 0; i < poemLines.length; i++) {
+        //   console.log(poemLines[i]);
+        // }
 
-        // loops through the lines array
-        for (var i = 0; i < poemLines.length; i++) {
-          console.log(poemLines[i]);
-// >>>>>>> main
-        }
-      }
-
-      else {
-        console.log("No poem found corresponding to your search term(s)")      
-      }
-
-    });
-  }
-  // Advance search
-  else {
-
-    // search by author + title
-    if (advAuthor !== "" && advTitle !== "" && advLines === "") {
-
-      advAuthor.replace(" ", "%");
-      advTitle.replace(" ", "%");
-      queryURL = `https://poetrydb.org/author,title,poemcount/${advAuthor};${advTitle};1`;
-    
-    }
-
-    // search by author + lines
-    else if (advAuthor !== "" && advTitle === "" && advLines !== "") {
-
-      advAuthor.replace(" ", "%");
-      advLines.replace(" ", "%");
-      queryURL = `https://poetrydb.org/author,lines,poemcount/${advAuthor};${advLines};1`;
-
-    }
-
-    // search by title + lines
-    else if (advAuthor === "" && advTitle !== "" && advLines !== "") {
-
-      advTitle.replace(" ", "%");
-      advLines.replace(" ", "%");
-      queryURL = `https://poetrydb.org/title,lines,poemcount/${advTitle};${advLines};1`;
-    
-    }
-  
-
-    // Call to the API using one of the queryURL value above
-    $.ajax({
-      url: queryURL,
-      method: "GET",
-    }).then(function (response) {
-      if (typeof response === Array) {
-        console.log(response);
-
-        // gets the poem's title
-        var poemTitle = response[0].title;
-        console.log("Title: " + poemTitle);
-
-        // gets the poem's author
-        var poemAuthor = response[0].author;
-        console.log("Author: " + poemAuthor);
-
-        // gets the poem's lines
-        var poemLines = response[0].lines;
-
-        console.log("Lines: ");
-
-        // loops through the lines array
-        for (var i = 0; i < poemLines.length; i++) {
-          console.log(poemLines[i]);
-        }
-      }
-
-      else {
-        console.log("No poem found corresponding to your search term(s)");
+        renderPoem(response);
       }
     });
   }
-
-  // resets the search boxes
-  $("#main-input").val("");
-  $("#adv-author").val("");
-  $("#adv-title").val("");
-  $("#adv-lines").val("");
-
 }
 
 // PoetryDB API path to get random poem: https://poetrydb.org/random
@@ -195,10 +100,37 @@ $("#randomPoem").on("click", function () {
   }).then(function (response) {
     // console.log("random poem", response);
     // log poem fields to console when "find me a poem" link is clicked from navbar
-    console.log(
-      `title: ${response[0].title}
-      \nauthor: ${response[0].author}
-      \npoem: ${response[0].lines}`
-    );
+    // console.log(
+    //   `title: ${response[0].title}
+    //   \nauthor: ${response[0].author}
+    //   \npoem: ${response[0].lines}`
+    // );
+
+    renderPoem(response);
   });
 });
+
+// displays poem in the poem card
+function renderPoem(response) {
+  // gets the poem's title
+  var poemTitle = response[0].title;
+  // adds title to the poem card
+  $(".POTD-Title").text(poemTitle);
+
+  // gets the poem's author
+  var poemAuthor = response[0].author;
+  // adds author to the poem card
+  $(".POTD-Author").text(poemAuthor);
+
+  // gets the poem's lines
+  var poemLines = response[0].lines;
+
+  // loops through the lines array and add each lines to the poem card
+  for (var i = 0; i < poemLines.length; i++) {
+    // $(".Poem-Text").text(poemLines[0])
+    $(".POTD-Author").after(
+      `<p class="Poem-Text d-flex justify-content-center">${poemLines[i]}</p>`
+    );
+    // $(".Poem-Text").attr("class", "justify-content-center")
+  }
+}
